563f88977fd3f1fad80a322c8c97d915a432d4c2
Warren Haskins <warrenhaskins1@gmail.com>
2021-12-09 18:31:57 -0500

    restart

--- script3.js
+++ script3.js
@@ -1,36 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
-
-}
 
 function generatePassword() {
-
+    //Get vars for our char types
     var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lower = "abcdefghijklmnopqrstuvwxyz";
     var num = "0123456789";
-    var special = "!@#$%^&*";
-    
+    var special = "~`!@#$%^&*()+?";
+    var selected = " ";
 
-    //We need to convert array to num for pwLength
+
+    //We need to convert array to num for pwLength??
 
     //Start the Generator with a Welcome Message
     console.log(window.confirm("Welcome to the Javascript Password Generator! Choose from a group of criteria to be included in your new Password. Click OK to continue!"));
 
     //Select Y/N for upper case letters
     function selectUpper() {
-        var selectUpper = confirm("Would you like to use Upper Case Letters in your Password?");
-        console.log(selectUpper);
-        if (!selectUpper) {
+        var selectedUpper = confirm("Would you like to use Upper Case Letters in your Password?");
+        console.log(selectedUpper);
+        if (!selectedUpper) {
             alert("You have selected No upper case letters");
         } else {
             alert("You have selected Yes to upper case letters");
+        }
+        //We need to make sure our types are valid (if conditional??--> if (condition){then})
+        if (selectedUpper) {
+            selected = upper;
         }
     }
     //Call above function
@@ -38,12 +36,16 @@ function generatePassword() {
 
     //Select Y/N for lower case letters
     function selectLower() {
-        var selectLower = confirm("Would you like to use Lower Case Letters in your Password Y/N?");
-        console.log(selectLower);
-        if (!selectLower) {
+        var selectedLower = confirm("Would you like to use Lower Case Letters in your Password Y/N?");
+        console.log(selectedLower);
+        if (!selectedLower) {
             alert("You have selected No lower case letters");
         } else {
             alert("You have selected Yes to lower case letters");
+        }
+        //We need to make sure our types are valid (if conditional??--> if (condition){then})
+        if (selectedLower) {
+            selected = lower;
         }
     }
     //Call above function
@@ -51,12 +53,16 @@ function generatePassword() {
 
     //Select Y/N for numbers
     function selectNum() {
-        var selectNum = confirm("Would you like to use numbers in your Password Y/N?");
-        console.log(selectNum);
-        if (!selectNum) {
+        var selectedNum = confirm("Would you like to use numbers in your Password Y/N?");
+        console.log(selectedNum);
+        if (!selectedNum) {
             alert("You have selected No Numbers");
         } else {
             alert("You have selected Yes to Numbers");
+        }
+        //We need to make sure our types are valid (if conditional??--> if (condition){then})
+        if (selectedNum) {
+            selected = num;
         }
     }
     //Call above function
@@ -64,20 +70,20 @@ function generatePassword() {
 
     //Select Y/N for specialCharacters
     function selectSpecial() {
-        var selectSpecial = confirm("Would you like to use Special Characters in your Password Y/N?");
-        console.log(selectSpecial);
-        if (!selectSpecial) {
+        var selectedSpecial = confirm("Would you like to use Special Characters in your Password Y/N?");
+        console.log(selectedSpecial);
+        if (!selectedSpecial) {
             alert("You have selected No special characters");
         } else {
             alert("You have selected Yes to special characters");
         }
+        //We need to make sure our types are valid (if conditional??--> if (condition){then})
+        if (selectedSpecial) {
+            selected = special;
+        }
     }
     //Call above function
     selectSpecial();
-
-    //We need to make sure our types are valid (if conditional??)
-    
-
 
     //Select character length for password
     function getPasswordLength() {
@@ -93,8 +99,21 @@ function generatePassword() {
     //Call above function
     getPasswordLength();
 
+
+    //We need to loop through random char that we get from user confirm and to the users selected pwLength. Use for iteration (i=0;<length;i++) and Math.Random from PRS.
+    for (var i = 0; i < passwordLength; i++) {
+        console.log(passwordLength);
+    }
+
+
+    // Write password to the #password input
+    function writePassword() {
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
+
+        passwordText.value = password;
+
+    }
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
